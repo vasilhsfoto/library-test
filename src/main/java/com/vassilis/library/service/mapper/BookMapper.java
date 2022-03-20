3926754c982062e1bdbb8ba406b306ebe4869d98
Vassilis Fotopoulos <Vasileios.Fotopoulos@citrix.com>
2022-03-20 20:13:07 +0100

    rever

--- src/main/java/com/vassilis/library/service/mapper/BookMapper.java
+++ src/main/java/com/vassilis/library/service/mapper/BookMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.Mappings;
 
 import java.util.List;
 
-@Mapper
+@Mapper(collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE)
 public abstract class BookMapper {
 
     @Mappings(
